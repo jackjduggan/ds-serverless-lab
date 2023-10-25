856fe6d566a8eb21354fd8d6622a64e50eea2ba2
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2023-10-25 10:15:29 +0100

    Add new movie endpoint.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -54,6 +54,18 @@ export class RestAPIStack extends cdk.Stack {
           },
         }
         );
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });    
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -74,6 +86,7 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "demo api",
@@ -93,6 +106,10 @@ export class RestAPIStack extends cdk.Stack {
     moviesEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
+    );
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
     );
 
     const movieEndpoint = moviesEndpoint.addResource("{movieId}");
